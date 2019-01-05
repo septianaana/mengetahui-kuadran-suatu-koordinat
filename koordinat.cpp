1ecd55d7aea6a440ff1c737019ce8511d0458617
septianaana <45724156+septianaana@users.noreply.github.com>
2019-01-05 15:15:42 +0700

    Add files via upload

---
+++ koordinat.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<stdio.h>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    int x,y;
+    cout << "\t\t ================== \n";
+    cout << "\t\t Program koordinat \n";
+    cout << "\t\t ================== \n";
+    cout << "\t\t Masukkan nilai x :";cin>>x;
+    cout << "\t\t Masukkan nilai y :";cin>>y;
+
+    if(x>0&&y>0)
+        printf("\t\t KUADRAN 1");
+    if(x>0&&y<0)
+        printf("\t\t KUADRAN 2");
+    if(x<0&&y<0)
+        printf("\t\t KUADRAN 3");
+    if(x<0&&y>0)
+        printf("\t\t KUADRAN 4");
+    if(x==0&&y==0)
+        printf("\t\t TITIK PUSAT");
+    getch ();
+}
